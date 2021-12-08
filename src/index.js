23ce62c0968bffe7fa98782a8e8a8ef55ae9657f
alessandrordgs <ale.silva1120@gmail.com>
2021-12-07 22:08:52 -0300

    refator

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-
+require('dotenv').config()
 server.use(middlewares)
 server.use(router)
-server.listen(8080, () => {
+server.listen(process.env.PORT, () => {
   console.log('Server is running')
 })
